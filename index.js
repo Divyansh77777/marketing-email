3d85b47eecb948d32238ba40c816b7607d4fe4d1
Divyansh <divyanshgoel777@gmail.com>
2023-12-27 16:51:55 +0530

    Updates

--- index.js
+++ index.js
@@ -72,6 +72,6 @@ app.post('/send-email', (req, res) => {
 });
 
 // Start the server
-app.listen(process.env.port, () => {
-    console.log(`Server is running on http://localhost:`+ process.env.port);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:`+ process.env.PORT);
 });
